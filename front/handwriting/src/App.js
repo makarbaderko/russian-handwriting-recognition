74c7745f7c9fe0296c5020a7dfc32d63ed89f8e2
Makar Baderko <makarbaderko@gmail.com>
2022-02-10 22:02:21 +0300

    Added drawing field and unstyled button

--- front/handwriting/src/App.js
+++ front/handwriting/src/App.js
@@ -1,6 +1,25 @@
 /* eslint-disable jsx-a11y/alt-text */
 import './App.css'
 import bg from './assets/bg.jpg';
+import { ReactSketchCanvas } from 'react-sketch-canvas';
+
+const styles = {
+  position: 'absolute',
+  marginTop: '30%',
+  marginLeft: '-25%'
+};
+
+const Canvas = () => {
+  return (
+    <ReactSketchCanvas
+      style={styles}
+      width="400"
+      height="600"
+      strokeWidth={4}
+      strokeColor="red"
+    />
+  );
+};
 
 function App() {
   return (
@@ -10,7 +29,10 @@ function App() {
       <body className='body'>
         <div className='mainDiv'>
           <img className='bg' src={bg}></img>
-          <p className='test'>Draw a Cyrillic Letter</p>
+          <p className='heading'>Draw a Cyrillic Letter</p>
+          <Canvas></Canvas>
+          <button className='btn'>Predict</button>
+          <p className='text'>It is letter</p>
         </div>
       </body>
     </div>
