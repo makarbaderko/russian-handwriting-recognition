cab3f69c6d0df12c95c0078eecfaa28a30d2ba8f
Makar Baderko <makarbaderko@gmail.com>
2022-02-11 11:02:18 +0300

    Written bash script to reformat image data

---
+++ back/move.sh
@@ -0,0 +1,133 @@
+#!/bin/bash
+mv  -v data/archive/01_20_00/01_20_00/* data/archive/01_20_00/
+rm -r data/archive/01_20_00/01_20_00
+mv  -v data/archive/01_14_00/01_14_00/* data/archive/01_14_00/
+rm -r data/archive/01_14_00/01_14_00
+mv  -v data/archive/01_04_00/01_04_00/* data/archive/01_04_00/
+rm -r data/archive/01_04_00/01_04_00
+mv  -v data/archive/01_30_00/01_30_00/* data/archive/01_30_00/
+rm -r data/archive/01_30_00/01_30_00
+mv  -v data/archive/01_10_00/01_10_00/* data/archive/01_10_00/
+rm -r data/archive/01_10_00/01_10_00
+mv  -v data/archive/01_24_00/01_24_00/* data/archive/01_24_00/
+rm -r data/archive/01_24_00/01_24_00
+mv  -v data/archive/01_00_00/01_00_00/* data/archive/01_00_00/
+rm -r data/archive/01_00_00/01_00_00
+mv  -v data/archive/01_11_00/01_11_00/* data/archive/01_11_00/
+rm -r data/archive/01_11_00/01_11_00
+mv  -v data/archive/01_25_00/01_25_00/* data/archive/01_25_00/
+rm -r data/archive/01_25_00/01_25_00
+mv  -v data/archive/01_01_00/01_01_00/* data/archive/01_01_00/
+rm -r data/archive/01_01_00/01_01_00
+mv  -v data/archive/01_21_00/01_21_00/* data/archive/01_21_00/
+rm -r data/archive/01_21_00/01_21_00
+mv  -v data/archive/01_15_00/01_15_00/* data/archive/01_15_00/
+rm -r data/archive/01_15_00/01_15_00
+mv  -v data/archive/01_05_00/01_05_00/* data/archive/01_05_00/
+rm -r data/archive/01_05_00/01_05_00
+mv  -v data/archive/01_31_00/01_31_00/* data/archive/01_31_00/
+rm -r data/archive/01_31_00/01_31_00
+mv  -v data/archive/00_24_00/00_24_00/* data/archive/00_24_00/
+rm -r data/archive/00_24_00/00_24_00
+mv  -v data/archive/00_10_00/00_10_00/* data/archive/00_10_00/
+rm -r data/archive/00_10_00/00_10_00
+mv  -v data/archive/00_00_00/00_00_00/* data/archive/00_00_00/
+rm -r data/archive/00_00_00/00_00_00
+mv  -v data/archive/00_14_00/00_14_00/* data/archive/00_14_00/
+rm -r data/archive/00_14_00/00_14_00
+mv  -v data/archive/00_20_00/00_20_00/* data/archive/00_20_00/
+rm -r data/archive/00_20_00/00_20_00
+mv  -v data/archive/00_30_00/00_30_00/* data/archive/00_30_00/
+rm -r data/archive/00_30_00/00_30_00
+mv  -v data/archive/00_04_00/00_04_00/* data/archive/00_04_00/
+rm -r data/archive/00_04_00/00_04_00
+mv  -v data/archive/00_15_00/00_15_00/* data/archive/00_15_00/
+rm -r data/archive/00_15_00/00_15_00
+mv  -v data/archive/00_21_00/00_21_00/* data/archive/00_21_00/
+rm -r data/archive/00_21_00/00_21_00
+mv  -v data/archive/00_31_00/00_31_00/* data/archive/00_31_00/
+rm -r data/archive/00_31_00/00_31_00
+mv  -v data/archive/00_05_00/00_05_00/* data/archive/00_05_00/
+rm -r data/archive/00_05_00/00_05_00
+mv  -v data/archive/00_25_00/00_25_00/* data/archive/00_25_00/
+rm -r data/archive/00_25_00/00_25_00
+mv  -v data/archive/00_11_00/00_11_00/* data/archive/00_11_00/
+rm -r data/archive/00_11_00/00_11_00
+mv  -v data/archive/00_01_00/00_01_00/* data/archive/00_01_00/
+rm -r data/archive/00_01_00/00_01_00
+mv  -v data/archive/01_18_00/01_18_00/* data/archive/01_18_00/
+rm -r data/archive/01_18_00/01_18_00
+mv  -v data/archive/00_16_00/00_16_00/* data/archive/00_16_00/
+rm -r data/archive/00_16_00/00_16_00
+mv  -v data/archive/00_22_00/00_22_00/* data/archive/00_22_00/
+rm -r data/archive/00_22_00/00_22_00
+mv  -v data/archive/00_32_00/00_32_00/* data/archive/00_32_00/
+rm -r data/archive/00_32_00/00_32_00
+mv  -v data/archive/00_06_00/00_06_00/* data/archive/00_06_00/
+rm -r data/archive/00_06_00/00_06_00
+mv  -v data/archive/01_08_00/01_08_00/* data/archive/01_08_00/
+rm -r data/archive/01_08_00/01_08_00
+mv  -v data/archive/01_28_00/01_28_00/* data/archive/01_28_00/
+rm -r data/archive/01_28_00/01_28_00
+mv  -v data/archive/00_26_00/00_26_00/* data/archive/00_26_00/
+rm -r data/archive/00_26_00/00_26_00
+mv  -v data/archive/00_12_00/00_12_00/* data/archive/00_12_00/
+rm -r data/archive/00_12_00/00_12_00
+mv  -v data/archive/00_02_00/00_02_00/* data/archive/00_02_00/
+rm -r data/archive/00_02_00/00_02_00
+mv  -v data/archive/01_29_00/01_29_00/* data/archive/01_29_00/
+rm -r data/archive/01_29_00/01_29_00
+mv  -v data/archive/00_27_00/00_27_00/* data/archive/00_27_00/
+rm -r data/archive/00_27_00/00_27_00
+mv  -v data/archive/00_13_00/00_13_00/* data/archive/00_13_00/
+rm -r data/archive/00_13_00/00_13_00
+mv  -v data/archive/00_03_00/00_03_00/* data/archive/00_03_00/
+rm -r data/archive/00_03_00/00_03_00
+mv  -v data/archive/01_19_00/01_19_00/* data/archive/01_19_00/
+rm -r data/archive/01_19_00/01_19_00
+mv  -v data/archive/00_17_00/00_17_00/* data/archive/00_17_00/
+rm -r data/archive/00_17_00/00_17_00
+mv  -v data/archive/00_23_00/00_23_00/* data/archive/00_23_00/
+rm -r data/archive/00_23_00/00_23_00
+mv  -v data/archive/00_07_00/00_07_00/* data/archive/00_07_00/
+rm -r data/archive/00_07_00/00_07_00
+mv  -v data/archive/01_09_00/01_09_00/* data/archive/01_09_00/
+rm -r data/archive/01_09_00/01_09_00
+mv  -v data/archive/01_12_00/01_12_00/* data/archive/01_12_00/
+rm -r data/archive/01_12_00/01_12_00
+mv  -v data/archive/00_28_00/00_28_00/* data/archive/00_28_00/
+rm -r data/archive/00_28_00/00_28_00
+mv  -v data/archive/01_26_00/01_26_00/* data/archive/01_26_00/
+rm -r data/archive/01_26_00/01_26_00
+mv  -v data/archive/01_02_00/01_02_00/* data/archive/01_02_00/
+rm -r data/archive/01_02_00/01_02_00
+mv  -v data/archive/01_22_00/01_22_00/* data/archive/01_22_00/
+rm -r data/archive/01_22_00/01_22_00
+mv  -v data/archive/00_18_00/00_18_00/* data/archive/00_18_00/
+rm -r data/archive/00_18_00/00_18_00
+mv  -v data/archive/01_16_00/01_16_00/* data/archive/01_16_00/
+rm -r data/archive/01_16_00/01_16_00
+mv  -v data/archive/01_06_00/01_06_00/* data/archive/01_06_00/
+rm -r data/archive/01_06_00/01_06_00
+mv  -v data/archive/00_08_00/00_08_00/* data/archive/00_08_00/
+rm -r data/archive/00_08_00/00_08_00
+mv  -v data/archive/01_32_00/01_32_00/* data/archive/01_32_00/
+rm -r data/archive/01_32_00/01_32_00
+mv  -v data/archive/01_23_00/01_23_00/* data/archive/01_23_00/
+rm -r data/archive/01_23_00/01_23_00
+mv  -v data/archive/00_19_00/00_19_00/* data/archive/00_19_00/
+rm -r data/archive/00_19_00/00_19_00
+mv  -v data/archive/01_17_00/01_17_00/* data/archive/01_17_00/
+rm -r data/archive/01_17_00/01_17_00
+mv  -v data/archive/01_07_00/01_07_00/* data/archive/01_07_00/
+rm -r data/archive/01_07_00/01_07_00
+mv  -v data/archive/00_09_00/00_09_00/* data/archive/00_09_00/
+rm -r data/archive/00_09_00/00_09_00
+mv  -v data/archive/01_13_00/01_13_00/* data/archive/01_13_00/
+rm -r data/archive/01_13_00/01_13_00
+mv  -v data/archive/00_29_00/00_29_00/* data/archive/00_29_00/
+rm -r data/archive/00_29_00/00_29_00
+mv  -v data/archive/01_27_00/01_27_00/* data/archive/01_27_00/
+rm -r data/archive/01_27_00/01_27_00
+mv  -v data/archive/01_03_00/01_03_00/* data/archive/01_03_00/
+rm -r data/archive/01_03_00/01_03_00
